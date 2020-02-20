add70b0d89713fdbf017e2f84fce75d808a1c909
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-02-20 22:55:44 +0100

    css grid & button value display

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const operate = (op, a, b) => console.log(op(a, b));
+const operate = (op, a, b) => op(a, b);
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const display = document.querySelector('#display');
+const button = document.querySelectorAll('.button');
 
-const value = document.querySelectorAll('.button').forEach(button =>
-  button.addEventListener('click', (e) => e.target.innerHTML));
+var value = button.forEach(btn => btn.addEventListener('click', (e) => document.querySelector('#display').value = (e.target.value)));
 
-console.log(value);
+// function press() {
+//   document.querySelectorAll('.button').forEach(button =>
+//     button.addEventListener('click', (e) => e.target.value));
+// }
 
-operate(add, 40, 2);
+// const display = document.querySelector('#display');
+// display.addEventListener(press());
+
+// console.log(value);
+
+operate(add, 40, 2)
