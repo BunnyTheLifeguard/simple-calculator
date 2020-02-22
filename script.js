2baaf934367b9c4f69ddf9cb9ddecbfc520a037b
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-02-22 16:09:00 +0100

    New approach

--- script.js
+++ script.js
@@ -1,22 +1,115 @@
-const operate = (op, a, b) => op(a, b);
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.keys')
+const display = document.querySelector('.display')
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+keys.addEventListener('click', (e) => {
+  if(e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    const previousKeyType = calculator.dataset.previousKeyType
 
-const button = document.querySelectorAll('.button');
+    Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
 
-var value = button.forEach(btn => btn.addEventListener('click', (e) => document.querySelector('#display').value = (e.target.value)));
+    if(!action) {
+      if(
+        displayedNum === '0' ||
+        previousKeyType === 'operator' ||
+        previousKeyType === 'calculate'
+        ) {
+        display.textContent = keyContent
+      } else {
+        display.textContent = display.textContent + keyContent
+      }
+      calculator.dataset.previousKeyType = 'number'
+    }
 
-// function press() {
-//   document.querySelectorAll('.button').forEach(button =>
-//     button.addEventListener('click', (e) => e.target.value));
-// }
+    if(
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+      ) {
+      const firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      const secondValue = displayedNum
 
-// const display = document.querySelector('#display');
-// display.addEventListener(press());
+      if(
+        firstValue &&
+        operator &&
+        previousKeyType !== 'operator'
+        && previousKeyType !== 'calculate'
+        ) {
+        const calcValue = calculate(firstValue, operator, secondValue)
+        display.textContent = calcValue
 
-// console.log(value);
+        calculator.dataset.firstValue = calcValue
+      } else {
+        calculator.dataset.firstValue = displayedNum
+      }
 
-operate(add, 40, 2)
+      key.classList.add('is-depressed')
+      calculator.dataset.previousKeyType = 'operator'
+      calculator.dataset.operator = action
+    }
+
+    if(action === 'decimal') {
+      if(!displayedNum.includes('.')) {
+        display.textContent = displayedNum + '.'
+      } else if(previousKeyType === 'operator' || previousKeyType === 'calculate') {
+        display.textContent = '0.'
+      }
+      calculator.dataset.previousKeyType = 'decimal'
+    }
+
+    if(action !== 'clear') {
+      const clearButton = calculator.querySelector('[data-action=clear]')
+      clearButton.textContent = 'CE'
+    }
+    if(action === 'clear') {
+      if(key.textContent === 'AC') {
+        calculator.dataset.firstValue = ''
+        calculator.dataset.modValue = ''
+        calculator.dataset.operator = ''
+        calculator.dataset.operator = ''
+      } else {
+        key.textContent = 'AC'
+      }
+
+      display.textContent = 0
+      calculator.dataset.previousKeyType = 'clear'
+    }
+
+    if(action === 'calculate') {
+      let firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      let secondValue = displayedNum
+
+      if(firstValue) {
+        if(previousKeyType === 'calculate') {
+          firstValue = displayedNum
+          secondValue = calculator.dataset.modValue
+        }
+        display.textContent = calculate(firstValue, operator, secondValue)
+      }
+      calculator.dataset.modValue = secondValue
+      calculator.dataset.previousKeyType = 'calculate'
+    }
+  }
+})
+
+const calculate = (n1, operator, n2) => {
+  let result = ''
+
+  if(operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if(operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if(operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if(operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+  return result
+}
